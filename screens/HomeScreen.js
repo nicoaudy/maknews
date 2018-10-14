b1882dba87e89d58ec6b486e16acd9acf59c199e
Cody Macbook <nicoaudy1994@gmail.com>
2018-10-14 08:25:20 +0700

    initial commit with home scaffold

---
+++ screens/HomeScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'MakNews',
+    headerStyle: {
+      backgroundColor: 'black' 
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold' 
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            <Text>Render Headline News</Text>
+          </View>
+
+          <View style={styles.getStartedContainer}>
+            <Text>Get started by opening</Text>
+          </View>
+
+        </ScrollView>
+
+      </View>
+    );
+  }
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  getStartedContainer: {
+    marginHorizontal: 10,
+  },
+  tabBarInfoContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+      },
+      android: {
+        elevation: 20,
+      },
+    }),
+  },
+});
